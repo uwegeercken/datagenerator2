d4e404eae7731f540616ee0f1a5944cfe81a9c6f
Uwe Geercken <uwe.geercken@web.de>
2023-07-05 09:38:25 +0200

    added tests for data transformer

---
+++ src/test/java/DataTransformerTest.java
@@ -0,0 +1,99 @@
+import com.datamelt.utilities.datagenerator.ConfigurationLoader;
+import com.datamelt.utilities.datagenerator.config.CategoryFileLoader;
+import com.datamelt.utilities.datagenerator.config.model.DataConfiguration;
+import com.datamelt.utilities.datagenerator.config.process.DataFieldsProcessor;
+import com.datamelt.utilities.datagenerator.generate.Row;
+import com.datamelt.utilities.datagenerator.generate.RowBuilder;
+import com.datamelt.utilities.datagenerator.utilities.transformation.DataTransformer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.awt.image.SinglePixelPackedSampleModel;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DataTransformerTest
+{
+    @Test
+    @DisplayName("testing reverse")
+    void validateReverseString() throws Exception
+    {
+        String input = "Hello World";
+        assertEquals("dlroW olleH", DataTransformer.reverse(input));
+    }
+
+    @Test
+    @DisplayName("testing uppercase")
+    void validateUppercaseString() throws Exception
+    {
+        String input = "Hello World";
+        assertEquals("HELLO WORLD", DataTransformer.uppercase(input));
+    }
+
+    @Test
+    @DisplayName("testing lowercase")
+    void validateLowercaseString() throws Exception
+    {
+        String input = "Hello World";
+        assertEquals("hello world", DataTransformer.lowercase(input));
+    }
+
+    @Test
+    @DisplayName("testing base64encode")
+    void validateBase64EncodedString() throws Exception
+    {
+        String input = "Hello World";
+        assertEquals("SGVsbG8gV29ybGQ=", DataTransformer.base64encode(input));
+    }
+
+    @Test
+    @DisplayName("testing prepend")
+    void validatePrefixedString() throws Exception
+    {
+        String input = "Hello World";
+        String prefix = "Message - ";
+        assertEquals("Message - Hello World", DataTransformer.prepend(input, prefix));
+    }
+
+    @Test
+    @DisplayName("testing append")
+    void validatePostfixedString() throws Exception
+    {
+        String input = "Hello World";
+        String postfix = " - Message";
+        assertEquals("Hello World - Message", DataTransformer.append(input, postfix));
+    }
+
+    @Test
+    @DisplayName("testing negate long")
+    void validateNegatedLong() throws Exception
+    {
+        long input = 1000;
+        assertEquals(-input, DataTransformer.negate(input));
+    }
+
+    @Test
+    @DisplayName("testing negate double")
+    void validateNegatedDouble() throws Exception
+    {
+        double input = 123.45;
+        assertEquals(-input, DataTransformer.negate(input));
+    }
+
+    @Test
+    @DisplayName("testing rounded double with two decimals")
+    void validateRoundedDouble() throws Exception
+    {
+        double input = 123.456789;
+        assertEquals(123.46, DataTransformer.round(input,2l));
+    }
+
+    @Test
+    @DisplayName("testing rounded double no decimals")
+    void validateRoundedDoubleNoDecimals() throws Exception
+    {
+        double input = 123.456789;
+        assertEquals(123, DataTransformer.round(input,0l));
+    }
+}
