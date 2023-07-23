b3d43c67fc52ebc781b7376056106203606d613d
uwegeercken <uwegeercken@web.de>
2023-07-23 17:41:41 +0200

    updated version

--- src/main/java/com/datamelt/utilities/datagenerator/DataGenerator.java
+++ src/main/java/com/datamelt/utilities/datagenerator/DataGenerator.java
@@ -21,8 +21,8 @@ public class DataGenerator
 {
     private static Logger logger = LoggerFactory.getLogger(DataGenerator.class);
     private static final String applicationName = "datagenerator2";
-    private static final String version = "0.0.8";
-    private static final String versionDate = "2023-07-09";
+    private static final String version = "0.1.0";
+    private static final String versionDate = "2023-07-23";
     private static final String contactEmail = "uwe.geercken@web.de";
     private DataConfiguration dataConfiguration;
     private ProgramConfiguration programConfiguration;
